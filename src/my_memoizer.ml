baf2b98cb9b1792e342a6330356140c1d6e87664
Anders Miltner <Anders.Miltner@gmail.com>
2022-05-18 13:02:52 -0500

    update memoizer

--- src/my_memoizer.ml
+++ src/my_memoizer.ml
@@ -8,17 +8,21 @@ module type UnfixedDataFunction = sig
   val f : (Arg.t -> Result.t) unfixed
 end
 
-module FixMemoizerOf(F:UnfixedDataFunction) = struct
+module FixMemoizerOf(F:UnfixedDataFunction) : Container
+  with type t = F.Arg.t -> F.Result.t =
+struct
   module ResultDict = DictOf(F.Arg)(F.Result)
 
   let result_storage : ResultDict.t ref = ref ResultDict.empty
 
-  let rec evaluate
+  type t = F.Arg.t -> F.Result.t
+
+  let rec v
       (x:F.Arg.t)
     : F.Result.t =
     begin match ResultDict.lookup (!result_storage) x with
       | None ->
-        let y = F.f evaluate x in
+        let y = F.f v x in
         result_storage := ResultDict.insert (!result_storage) x y;
         y
       | Some y -> y
